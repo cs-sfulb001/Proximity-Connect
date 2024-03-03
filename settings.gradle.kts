fb8f2a8b1e292427bcee244803039fc4a932018a
jonrossy1472 <jross015@odu.edu>
2024-03-03 13:39:57 -0500

    git init git add git commit -m "first commit" git branch -M main git remote add origin https://github.com/cs-sfulb001/Proximity-Connect git push -u origin main

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Proximity Connect login page attempt 3"
+include(":app")
+ 
