b836189ff2999a03eb396ecc4ab60ed388f95617
Brianna Spigner <bspig001@odu.edu>
2024-03-10 12:33:18 -0400

    additional sound files for testing and demo

--- app/src/main/python/transcription.py
+++ app/src/main/python/transcription.py
@@ -4,6 +4,8 @@ aai.settings.api_key = "353ff195df9d4243b247c2ecb4b80b46"
 
 USERNAME = "Brianna"
 
+clips = ["Ford", "Volvo", "BMW"]
+
 # URL of the file to transcribe
 FILE_URL = r"C:\Users\Brianna\Desktop\SeniorProject\Proximity-Connect\app\src\main\python\burger-king-foot-lettuce.mp3"
 
