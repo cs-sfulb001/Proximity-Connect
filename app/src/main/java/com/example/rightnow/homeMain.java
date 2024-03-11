8d9eea082da17190dfedb595e7fe0367fd68f6e1
Khama <kjeff008@odu.edu>
2024-03-11 18:57:37 -0400

    intial commit of right now

---
+++ app/src/main/java/com/example/rightnow/homeMain.java
@@ -0,0 +1,14 @@
+package com.example.rightnow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homeMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_main);
+    }
+}
