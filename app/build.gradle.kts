286d82c1b9779857ca1f1e02a87579ed817aab4f
Charles Milroy Jr <charlesmilroy@hotmail.com>
2024-03-12 10:59:03 -0400

    Fixed errors in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,61 +23,13 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
-dependencies {
-    val fragment_version ="1.6.1"
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    // https://mvnrepository.com/artifact/mysql/mysql-connector-java
-    implementation("mysql:mysql-connector-java:8.0.33")
-    //hamcrest
-    testImplementation("org.hamcrest:hamcrest-library:2.2")
-    implementation("androidx.fragment:fragment:$fragment_version")
-}plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-
-
-android {
-    namespace = "com.example.proximity_connect"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.proximity_connect"
-        minSdk = 27
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -99,12 +51,10 @@ android {
 }
 
 dependencies {
-
     val fragment_version ="1.6.1"
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     // https://mvnrepository.com/artifact/mysql/mysql-connector-java
@@ -145,8 +95,6 @@ dependencies {
     implementation("androidx.activity:activity:1.9.0-alpha03")
     implementation("androidx.activity:activity:1.9.0-alpha03")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
