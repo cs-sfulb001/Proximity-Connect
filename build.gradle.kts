4f1b2c823de9274941f597e2c8ef3b2ba2796876
Khama <kjeff008@odu.edu>
2024-03-17 18:35:17 -0400

    fixing merge errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-<<<<<<< HEAD
-    id("com.android.application") version "8.2.1" apply false
-=======
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
->>>>>>> 0fdb45655527abd588bf4de80e0f4f09630d9d73
+
 }
